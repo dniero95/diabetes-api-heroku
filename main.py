5b77d89057567c4e019f8e8828b5ca0138012ba8
dniero95 <darioniero@icloud.com>
2023-01-08 17:24:37 +0100

    add code to handel CORS

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle as pk
 import json
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class model_input(BaseModel):
     Pregnancies :int
